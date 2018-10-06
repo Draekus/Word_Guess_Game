e6039e4ce1695240c4e5e865703246d6108d6db5
Patrick Gould <patrick.gould94@gmail.com>
2018-10-06 16:26:36 -0400

    Final Changes Made To HTML, CSS, Images, & Javascript.

---
+++ assets/javascript/game.js
@@ -0,0 +1,86 @@
+var wordBank = ["despicably", "flashpoint", "formidable", "downstream", "slumbering", "infamously", "binoculars"]
+var computerChoice = wordBank[Math.floor(Math.random() * wordBank.length)]
+var winsArray = [];
+var wins = 0
+var prevGuesses = []
+var guessesLeft = 6
+
+
+
+$(document).ready(function() {
+    
+    $("#guessesleftvalue").text(guessesLeft)
+    $("#wins").text(wins)
+
+    for ( var i = 0; i < computerChoice.length; i++) {
+        var wordblocks = $("<p>")
+        wordblocks.addClass("wordblock" + i);
+        wordblocks.addClass("block")
+        wordblocks.addClass(computerChoice[i])
+        // wordblocks.attr("letter", computerChoice[i]);
+        $("#wordblanks").append(wordblocks);
+        console.log(computerChoice)
+    
+    
+}
+
+
+    
+
+    document.onkeyup = function(event) {
+    
+        $("#secondarytitle").text("");
+        var userGuess = event.key;
+        
+
+        
+        
+        if ( prevGuesses.includes(userGuess) ) {
+            console.log("You already guessed that!")
+            alert("Silly Goose You already guessed that!")
+            }
+
+        else if (computerChoice.includes(userGuess)) {
+            
+            console.log("You guessed right!");
+            $("." + userGuess).text(userGuess);
+            prevGuesses.push(userGuess);
+            $("#guessedletters").text(prevGuesses);
+            console.log(prevGuesses);
+            wins++;
+            $("#wins").text(wins)
+            winsArray.push(userGuess);
+            console.log(winsArray.length)
+            
+            
+
+            
+        }
+
+        else {
+            $("#guessedletters").text(userGuess);
+            prevGuesses.push(userGuess);
+            $("#guessedletters").text(prevGuesses);
+            guessesLeft--;
+            $("#guessesleftvalue").text(guessesLeft);
+            console.log(guessesLeft);
+
+        }
+
+        
+        if (winsArray.length === computerChoice.length) {
+            alert("Hey You Won!")
+        }
+        
+        else if (guessesLeft === 0) {
+            alert("Sorry you lost!")
+        }
+        
+
+        
+
+        
+
+    }
+});   
+    
